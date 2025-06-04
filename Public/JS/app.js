e6df579324b7d60a5911cc126f5c3b511974d2a8
Reha Demircan <153535205+rdmrcn@users.noreply.github.com>
2025-06-04 03:55:11 +0300

    Add files via upload

---
+++ Public/JS/app.js
@@ -0,0 +1,74 @@
+// select document ellements
+const form = document.querySelector('form');
+const address = document.querySelector('.search');
+const weatherContainer = document.querySelector('.weather-container');
+const weather_Error = document.querySelector('#error');
+const weather_Success = document.querySelector('#success');
+const spinner =document.querySelector('.spin');
+
+
+//create elements
+const weather_icon = document.createElement('img');
+const weather_area = document.createElement('p');
+const weather_condition = document.createElement('p');
+const weather_country = document.createElement('p');
+const weather_temp = document.createElement('p');
+const weather_error = document.createElement('p');
+
+// Add classes
+weather_icon.className='weather__icon';
+weather_area.className='weather__area';
+weather_condition.className='weather__condition';
+weather_country.className='weather__country';
+weather_temp.className='weather__temperature';
+weather_error.className='weather__error';
+
+
+
+form.addEventListener('submit',(e)=>{
+    // prevent page from reloading
+    e.preventDefault();
+
+    const city = address.value;
+    spinner.id='spinner';
+
+    // Clear other elements inputs
+    weather_error.innerHTML = '';
+    weather_Success.innerHTML =''
+
+    fetch(`/weather?address=${city}`)
+        .then(response=>response.json())
+        .then(data=>{
+            if(data.error){
+                spinner.id='';
+                weather_error.innerText = data.error;
+                //Append error container
+                weatherContainer.appendChild(weather_Error)
+                weather_Error.appendChild(weather_error);
+
+                // Remove previous forecast data when errors occur
+                weather_Success.innerHTML =''
+
+            }else{
+                //Clear any previous errors
+                weather_error.innerHTML= ''
+                //Clear form input
+                address.value = '';
+                spinner.id=''
+                const {city,condition,country,icon,region,temp} = data.forecast;
+                // Add image src
+                weather_icon.setAttribute('src',icon);
+
+                //Append text content
+                weather_area.innerText = city;
+                weather_condition.innerText= condition;
+                weather_country.innerText =`${region}, ${country}`;
+                weather_temp.innerHTML = `${temp} <sup>o</sup>C`;
+
+                // Append all weather elements to weatherContainer
+                weatherContainer.appendChild(weather_Success)
+                weather_Success.append(weather_icon,weather_temp,weather_condition,weather_area,weather_country)
+            }
+        })
+
+})
---
+++ src/app.js
@@ -0,0 +1,70 @@
+// Core Node Modules
+const path = require('path');
+
+// NPM modules
+const express = require('express');
+const hbs =require('hbs');
+require('dotenv').config({
+    path:'config/.env'
+});
+
+const geolocation = require('./utils/geolocation');
+const forecast = require('./utils/forecast');
+// Initialize app to use express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define paths to config express
+const staticAssetsPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+// Set app to use static assets
+app.use(express.static(staticAssetsPath));
+
+// set views engine and directory
+app.set('view engine','hbs');
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath);
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.render('index')
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/weather',(req,res)=>{
+    const city = req.query.address;
+    if(!city){
+        return res.send({
+            error:'You must provide an address'
+        })
+    }
+    geolocation(city,(error,{latitude,longitude}={})=>{
+        if(error){
+            return res.send({
+                error
+            })
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                forecast:forecastData
+            })
+        })
+
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('error')
+})
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
